9bb225d7ffd2abd6a189c31a2509e14738017573
Yuna Thúy Liễu <94217438+yunathuylieu@users.noreply.github.com>
2023-09-26 21:00:37 +0700

    EXERCISE 1 TO 8

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+
+    internal class Program
+    {
+        
+        static void ChangeValue(int x)
+        {
+            x = 200;
+            Console.WriteLine(x);
+        }
+            static void Main(string[] args)
+        {
+            int i = 100;
+            Console.WriteLine(i);
+
+            ChangeValue(i);
+
+            Console.WriteLine();
+            Console.Read();
+
+        }
+                }
+        }
+
+
